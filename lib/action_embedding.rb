92fcb410ac886af6e99eec9eeb62846d80e786f4
Ubuntu <ubuntu@ip-10-226-59-38.eu-west-1.compute.internal>
2010-06-30 11:07:45 +0000

    Collecting code from various places - don't expect this to do anything yet\!

--- lib/action_embedding.rb
+++ lib/action_embedding.rb
@@ -1,1 +1,47 @@
-# Just a placeholder... next commit will put something here!
+class FakeRequest < ActionController::AbstractRequest
+  attr_reader :path
+  attr_accessor :path_parameters
+  attr_accessor :session_options
+  attr_accessor :session
+  attr_accessor :cookies
+
+  def initialize(path, session, env)
+    @path = path
+    @session = session
+    @env = env
+    @cookies = {}
+    super()
+  end
+
+  def method
+    :get
+  end
+
+  def content_length
+    0
+  end
+
+  def query_string
+    ""
+  end
+
+  def user_agent
+    "Internal fake request agent"
+  end
+end
+
+class FakeResponse < ActionController::AbstractResponse
+  def out(output = $stdout, &block)
+    @body
+  end
+end
+
+ def embed_subpage(path)
+    new_request = FakeRequest.new(path, session.dup, request.env.dup)
+    new_response = FakeResponse.new
+
+    new_output = $stdout
+
+    Dispatcher.new(new_output, new_request, new_response).dispatch_unlocked
+  end
+
